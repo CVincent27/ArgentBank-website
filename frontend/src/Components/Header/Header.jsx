b87a467839218f85117c99ab062e7fe7b87a2f36
Cardon Vincent <cardon.vincent@hotmail.fr>
2024-06-04 14:40:59 +0200

    réupdate redux files

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,50 +1,107 @@
-import { NavLink, Link } from "react-router-dom";
-import logo from "../../assets/argentBankLogo.png";
-import { userResetAction } from "../../store/user";
-import { selectUserFirstName } from "../../store/selectors";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { updateUsername } from '../../redux/actions/user.actions.jsx';
+import { isValidName } from "../../utils/regex.jsx";
 
 function Header() {
+  const token = useSelector((state) => state.auth.token);
+    const userData = useSelector((state) => state.user.userData);
+    /* Manages the appearance of the username modification form */
+    const [display, setDisplay] = useState(true);
+    /* Get username */
+    const [userName, setUserName] = useState('');
+    /* Handle error message */
+    const [errorMessage, setErrorMessage] = useState('');
+
     const dispatch = useDispatch();
 
-    const userFirstName = useSelector(selectUserFirstName());
+    /* Asynchronous username update function */
+    const handleSubmitUsername = async (event) => {
+        event.preventDefault();
+        if (!isValidName(userName)) {
+            setErrorMessage("Invalid username");
+            return;
+        } else {
+            setErrorMessage("");
+        }
+        try {
+            const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+                body: JSON.stringify({userName}),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                const username = data.body.userName;
+                /* 
+                    Checking that the query response is indeed retrieved
+                    console.log(data) 
+                */
+                dispatch(updateUsername(username));
+                setDisplay(!display);
+            } else {
+                console.log("Invalid Fields")
+            }
 
-    // au click sur le btn de deco on appelle userResetAction 
-    // qui réinitialise l'état de l'utilisateur à null.
-    const signOut = () => {
-        dispatch(userResetAction());
-    };
+        } catch (error) {
+            console.error(error);
+        }
+    }
+  return (
+    <div className="header">
+    { display ?
+    <div>
+      <h1>Welcome back<br />
+      {userData.username} {userData.lastname} 
+      </h1>
 
-    return (
-        <nav className="main-nav">
-            <Link className="main-nav-logo" to="/">
-                <img className="main-nav-logo-image" src={logo} alt="Logo Argent Bank" />
-                <h1 className="sr-only">Argent Bank</h1>
-            </Link>
-                {!userFirstName && (
-                    <NavLink className="main-nav-item" to="/login">
-                        <i className="fa fa-user-circle"></i>
-                        <span> Sign In</span>
-                        
-                    </NavLink>
-                )}
-
-                {userFirstName && (
-                    <NavLink className="main-nav-item" to="/profile">
-                        <i className="fa fa-user-circle"></i>
-                        {!userFirstName && "Profil"}
-                        {userFirstName && userFirstName}
-                    </NavLink>
-                )}
-                {userFirstName && (
-                    <NavLink onClick={signOut} className="main-nav-item" to="/">
-                        <i className="fa fa-sign-out"></i>
-                        Sign Out
-                    </NavLink>
-                )}
-        </nav>
-    );
+      <button className="edit-button"onClick={() => setDisplay(!display)}>Edit Name</button>
+    </div>
+    :
+    <div>
+                    <h2>Edit user info</h2>
+                    <form>
+                        <div className="edit-input">
+                            <label htmlFor="username">User name:</label>
+                            <input
+                                type="text"
+                                id="username"
+                                defaultValue={userData.username}
+                                onChange={(event) => setUserName(event.target.value)}
+                            />
+                        </div>
+                        <div className="edit-input">
+                            <label htmlFor="firstname">First name:</label>
+                            <input
+                                type="text"
+                                id="firstname" 
+                                defaultValue={userData.firstname}
+                                disabled={true}
+                            />
+                        </div>
+                        <div className="edit-input">
+                            <label htmlFor="lastname">Last name:</label>
+                            <input
+                                type="text"
+                                id="lastname" 
+                                defaultValue={userData.lastname}
+                                disabled={true}
+                            />
+                        </div>
+                        <div className="buttons">
+                            <button className="edit-username-button" onClick={handleSubmitUsername}>Save</button>
+                            <button className="edit-username-button" onClick={() => setDisplay(!display)}>Cancel</button>
+                        </div>
+                        {errorMessage && <p className="error-message">{errorMessage}</p>}
+                    </form>
+                </div>
+            }
+        </div>
+    )
 }
+
 
 export default Header;
